3110e763761d857d7d483cd3fd0c3f0ba32f16df
vuld-2609 <87050972+vuld-2609@users.noreply.github.com>
2026-08-06 20:28:31 +0700

    Feat.deploy (#9)
    * feat: add schedule
    
    * feat: deploy

--- src/analytics/analytics.controller.ts
+++ src/analytics/analytics.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Query, UseGuards } from '@nestjs/common';
+import { Controller, Get, Post, Query, UseGuards } from '@nestjs/common';
 import { Role } from '@prisma/client';
 
 import { Roles } from '@/common/decorators/roles.decorator';
@@ -7,15 +7,24 @@ import { RolesGuard } from '@/guards/roles.guard';
 
 import { AnalyticsService } from './analytics.service';
 import { GetDashboardQueryDto } from './dto/get-dashboard-query-dto';
+import { MonthlyReportScheduler } from './monthly-report.scheduler';
 
 @Controller('analytics')
 @UseGuards(JwtAuthGuard, RolesGuard)
 @Roles(Role.ADMIN)
 export class AnalyticsController {
-  constructor(private readonly analyticsService: AnalyticsService) {}
+  constructor(
+    private readonly analyticsService: AnalyticsService,
+    private readonly monthReport: MonthlyReportScheduler,
+  ) {}
 
   @Get('dashboard')
   getDashboard(@Query() query: GetDashboardQueryDto) {
     return this.analyticsService.getDashboard(query.month, query.year);
+  }
+
+  @Post()
+  sendAnalyics() {
+    return this.monthReport.sendMonthlyReport();
   }
 }
